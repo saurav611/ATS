04dc5167c81495e58a5ad6571c9e29ef7fa901b5
Akash <akash.greenwork1@gmail.com>
2023-07-15 15:35:17 +0530

    dummy check again

--- src/components/Common/BottomNav.tsx
+++ src/components/Common/BottomNav.tsx
@@ -6,7 +6,7 @@ const bottomNavList = [
   { name: 'Setting', Logo: 'setting' },
   { name: 'Login', Logo: 'profile.gif', isGif: true }
 ];
-let sdsdsdsd = [];
+let sdsdsdsdddd = [];
 export default function BottomNav() {
   return (
     <div
