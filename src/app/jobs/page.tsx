d9e457d45ea7984bc160e60127207976aa4ecb91
Akash <akash.greenwork1@gmail.com>
2023-07-24 21:19:52 +0530

    feat: filter tab setup

---
+++ src/app/jobs/page.tsx
@@ -0,0 +1,12 @@
+import JobFilterTab from '@/components/Pages/jobs/JobFilterTab';
+import React from 'react';
+
+export default function Jobs() {
+  return (
+    <div className="p-7">
+      <div>
+        <JobFilterTab />
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-import Navbar from '@/components/Common/Navbar';
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between ">
-      <Navbar customClass={'mt-7'} />
-    </main>
+    <main className="flex min-h-screen flex-col items-center justify-between "></main>
   );
 }
